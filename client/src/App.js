555a110926558beb7553688e4cd32bb1fcfbed94
Rachana Patil <rachanapatil@MacBook-Pro.local>
2024-11-18 13:07:58 -0800

    auth context and login-register UI component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React,{Fragment} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import ContactState from './context/contact/ContactState';
-const App=() => {
-  return (
-    <ContactState>
-    <Router>
-    <Fragment>
-     <Navbar />
-     <div className="container">
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-      </Routes>
-     </div>
-    </Fragment>
-    </Router>
-    </ContactState>
-  );
-}
+import AuthState from './context/auth/AuthState';
+const App = () => {
+    return (
+        <AuthState>
+            <ContactState>
+                <Router>
+                    <Fragment>
+                        <Navbar />
+                        <div className="container">
+                            <Routes>
+                                <Route exact path="/" element={<Home />} />
+                                <Route exact path="/about" element={<About />} />
+                                <Route exact path="/register" element={<Register />} />
+                                <Route exact path="/login" element={<Login />} />
+                            </Routes>
+                        </div>
+                    </Fragment>
+                </Router>
+            </ContactState>
+        </AuthState>
+    );
+};
 
 export default App;
